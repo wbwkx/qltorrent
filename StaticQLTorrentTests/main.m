749c4114a61b881a57ac53cc9bc238b09cb9bf04
Frizlab <fload@me.com>
2011-11-20 01:52:58 +0100

    New target for testing purpose

---
+++ StaticQLTorrentTests/main.m
@@ -0,0 +1,17 @@
+/*
+ * main.m
+ * StaticQLTorrentTests
+ *
+ * Created by François LAMBOLEY on 11/20/11.
+ * Copyright (c) 2011 Sillage. All rights reserved.
+ */
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char *argv[]) {
+	@autoreleasepool {
+		
+	}
+	
+	return 0;
+}
